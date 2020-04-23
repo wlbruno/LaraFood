6fd0d08e78cb9caf0b35e246d3fb1b7817d78d0d
wlbruno <wlbn2121@gmail.com>
2020-04-23 20:59:26 -0300

    auth laravel+adminLTE 3

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -6,11 +6,11 @@ class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
-     *
+     *s
      * @return void
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+         $this->call(UsersTableSeeder::class);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 Route::prefix('admin')
 		->namespace('Admin')
+		->middleware('auth')
 		->group(function() {
 
 	/**
@@ -79,3 +80,6 @@ Route::prefix('admin')
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
