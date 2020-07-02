5d15a1fc774e6811c42d394e94e3b1bf0db0cfee
wlbruno willian <willianbruno.sespe@gmail.com>
2020-07-02 20:26:49 -0300

    exemple

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -27,6 +27,8 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if ($this->app->runningInConsole()) 
+        return;
         $this->registerPolicies();
 
         $permissions = Permission::all();
