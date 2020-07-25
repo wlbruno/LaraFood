69813893e6f610cba0cca582446969cef87c8b5a
wlbruno willian <willianbruno.sespe@gmail.com>
2020-07-24 21:43:52 -0300

    erro provider

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -27,6 +27,9 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if ($this->app->runningInConsole()) 
+        return;
+        
         $this->registerPolicies();
 
         $permissions = Permission::all();
